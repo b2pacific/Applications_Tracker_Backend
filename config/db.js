65e8fd5d525c13ea4641d7539b9dfff2b9533113
b2pacific <b2pacific@gmail.com>
2022-11-29 22:22:08 +0530

    Replaced environment variable with constant

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { MONGO_URL } = require("../utils/constants");
 
 if (process.env.NODE_ENV == "development") {
   exports.conn = mongoose.connect(
@@ -15,7 +16,7 @@ if (process.env.NODE_ENV == "development") {
   );
 } else {
   exports.conn = mongoose.connect(
-    process.env.MONGO_URL,
+    MONGO_URL,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
