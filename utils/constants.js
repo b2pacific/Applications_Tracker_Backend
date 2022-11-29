7ae3dfec52cf35a750784fd02867054bb63fab75
b2pacific <b2pacific@gmail.com>
2022-11-29 22:22:48 +0530

    Added expiry time constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,5 @@
+exports.MONGO_URL = process.env.MONGO_URL;
 exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 exports.JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+exports.REFRESH_TOKEN_EXPIRY = Number(process.env.REFRESH_TOKEN_EXPIRY);
+exports.ACCESS_TOKEN_EXPIRY = Number(process.env.ACCESS_TOKEN_EXPIRY);
