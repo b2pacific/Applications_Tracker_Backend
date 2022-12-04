e81c60ae3c34782a52ca19e85e17d7998608d8fb
b2pacific <b2pacific@gmail.com>
2022-12-04 20:19:43 +0530

    Added port const

--- utils/constants.js
+++ utils/constants.js
@@ -3,3 +3,4 @@ exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 exports.JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 exports.REFRESH_TOKEN_EXPIRY = Number(process.env.REFRESH_TOKEN_EXPIRY);
 exports.ACCESS_TOKEN_EXPIRY = Number(process.env.ACCESS_TOKEN_EXPIRY);
+exports.PORT = Number(process.env.PORT);
